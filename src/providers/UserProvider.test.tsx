1c17f0e92fdadf4fc3ae67b7839b88de97d78d90
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2022-08-24 22:10:27 -0400

    test(UserProvider): override handler

--- src/providers/UserProvider.test.tsx
+++ src/providers/UserProvider.test.tsx
@@ -1,17 +1,19 @@
 import { BrowserRouter } from "react-router-dom";
 import { act, renderHook } from "@testing-library/react-hooks";
 import { handlers } from "../msw/handlers";
+import { rest } from "msw";
 import { setupServer } from "msw/node";
 import { useUserContext } from "./UserProvider";
 
 const server = setupServer(...handlers);
 
-global.beforeEach(() => {
+beforeEach(() => {
     server.listen();
 });
 
-global.afterEach(() => {
+afterEach(() => {
     server.close();
+    
 });
 
 const mockUseNavigate = jest.fn();
@@ -24,17 +26,39 @@ jest.mock("react-router-dom", () => {
     };
 });
 
-describe("Auth", () => {
-    test("Should login and redirect the user to the cockpit page", async () => {
+describe("Login",  () => {
+    test("Should not redirect the user to the cockpit page", async () => {
+        server.use(
+            rest.post(
+                "http://localhost:8000/api/auth/secured-log-in",
+                async (req, res, ctx) => {
+                    return res.once(
+                        ctx.json({})
+                    );
+                }
+            )
+        )
         const { result, waitFor } = renderHook(useUserContext, { wrapper: BrowserRouter });
         expect(result.current.user).toEqual({ name: null, clientId: null });
         act(() => {
             result.current.handleLogin("admin@company.com");
         });
         await waitFor(() => {
-            expect(result.current.user).toEqual({ name: "admin", clientId: "73c40f94-2da8-441b-858f-c4756e6c83f5" });
+            expect(result.current.user).not.toEqual({ name: "admin", clientId: "73c40f94-2da8-441b-858f-c4756e6c83f5" });
         });
-        expect(mockUseNavigate).toHaveBeenCalledWith("/cockpit");
+        expect(mockUseNavigate).not.toHaveBeenCalledWith("/cockpit");
     });
-    
+
+    // test("Redirect the user to the cockpit page", async () => {
+    //     const { result, waitFor } = renderHook(useUserContext, { wrapper: BrowserRouter });
+    //     expect(result.current.user).toEqual({ name: null, clientId: null });
+    //     act(() => {
+    //         result.current.handleLogin("admin@company.com");
+    //     });
+    //     await waitFor(() => {
+    //         expect(result.current.user).toEqual({ name: "admin", clientId: "73c40f94-2da8-441b-858f-c4756e6c83f5" });
+    //     });
+    //     expect(mockUseNavigate).toHaveBeenCalledWith("/cockpit");
+    // });
 });
+
